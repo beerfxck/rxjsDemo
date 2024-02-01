81d095653ad5a3272545c0d38340b7335599f0ed
Teddymalaju123 <127287397+Teddymalaju123@users.noreply.github.com>
2024-02-02 01:52:43 +0700

    add

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,8 +12,26 @@ export class ProfileComponent {
   tasksData$!: Observable<any[]>;
   tasksIdData$!: Observable<any[]>;
   
-  tasksIdBySearchData$: Observable<any[]> | undefined;
+  tasksIdBySearchData$!: Observable<any>;
   onSearchTask = new Subject<number>();
+
+  // constructor(private dataService: ApiService) {
+  //   this.onSearchTask.pipe(
+  //     debounceTime(500),
+  //     switchMap((searchTaskId) => {
+  //       return this.dataService.getTasksIdBySearch(searchTaskId);
+  //     })
+  //   )
+  //     .subscribe((value) =>(this.tasksIdBySearchData$ = value)
+  //       // if (Array.isArray(value)) {
+  //       //   this.tasksIdBySearchData$ = of(value);
+  //       // } else if (typeof value === 'object' && value !== null) {
+  //       //   this.tasksIdBySearchData$ = of([value]);
+  //       // } else {
+  //       //   console.error('Invalid data returned:', value);
+  //       // }
+  //     );
+  // }
 
   constructor(private dataService: ApiService) {
     this.onSearchTask.pipe(
@@ -23,10 +41,8 @@ export class ProfileComponent {
       })
     )
       .subscribe((value) => {
-        if (Array.isArray(value)) {
+        if (typeof value === 'object' && value !== null) {
           this.tasksIdBySearchData$ = of(value);
-        } else if (typeof value === 'object' && value !== null) {
-          this.tasksIdBySearchData$ = of([value]);
         } else {
           console.error('Invalid data returned:', value);
         }
